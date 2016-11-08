e20c7b3ca4ede9724b28c6f955dc66c348bffec3
Linggarunkal <linggar@outlook.com>
2016-11-08 17:16:02 +0700

    edit file script.js

--- admin/js/script.js
+++ admin/js/script.js
@@ -126,3 +126,22 @@ $(document).ready(function () {
 
 
 // TO script.js and ajax user.html
+$(document).ready(function(){
+    var table_user = $("#show_user").DataTable({
+        "processing": true,
+        "serverSide": true,
+        "columnDefs": [{
+            "targets": 0,
+            "orderable": false,
+            "searchable": false
+        }],
+        "ajax": {
+            url: "ajax/data_user.php",
+            type: "post",
+            error: function(){
+                $(".show_user-error").html("");
+                $("#show_user").append('<tbody class="employee-grid-error"><tr><td>Data Tidak ditemukan</td></tr></tbody>')
+            }
+        }
+    })
+});
